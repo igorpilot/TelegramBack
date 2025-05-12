91e5d3cdd13b52289c46cef6f78ff9d5cf488ec7
igorpilot <ihorzhurba.air@gmail.com>
2025-05-12 16:27:13 +0200

    first commit

--- middlewares/error-middlewares.js
+++
@@ -1,8 +0,0 @@
-import ApiError from '../exceptions/api-error.js';
-
-export default function (err, req, res, next) {
-    if (err instanceof ApiError) {
-        return res.status(err.status).json({ message: err.message, errors: err.errors });
-    }
-    return res.status(500).json({ message: "Невідома помилка: " + err.message });
-}
--- models/token-model.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const TokenSchema = new mongoose.Schema({
-    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-    refreshToken: { type: String, required: true },
-});
-
-export default mongoose.model('TokenSchema', TokenSchema);
-
