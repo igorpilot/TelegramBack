a55f59e78b5e8e97a6d05e59a5b0be4a5a0c9dd4
igorpilot <ihorzhurba.air@gmail.com>
2025-05-14 16:50:11 +0200

    link

--- index.js
+++ index.js
@@ -8,55 +8,42 @@ import TelegramBot from 'node-telegram-bot-api';
 
 dotenv.config();
 
-// Встановлення порту та налаштування Express
 const PORT = process.env.PORT || 5000;
 const app = express();
 
-// Бот Telegram
-const token = process.env.TELEGRAM_BOT_TOKEN; // Встановлення токену з .env
+const token = process.env.TELEGRAM_BOT_TOKEN;
 const bot = new TelegramBot(token, { polling: true });
 
-// Налаштування дозволених доменів для CORS
-const allowedOrigins = ['http://localhost:3000', 'http://localhost:3001','https://fe9b-88-212-17-217.ngrok-free.app, https://proj-six-dun.vercel.app'];
+const allowedOrigins = ['http://localhost:3000', 'http://localhost:3001','https://fe9b-88-212-17-217.ngrok-free.app', 'https://proj-six-dun.vercel.app'];
 
-// Створення обробника команди /start
 bot.onText(/\/start/, (msg) => {
     const chatId = msg.chat.id;
 
-
-    // Відправка повідомлення з кнопкою
     bot.sendMessage(chatId, "Hi, are you ready for earn?", {
         reply_markup: {
             keyboard: keyboard,
-            resize_keyboard: true, // Щоб кнопка була адаптована під розмір екрану
-            one_time_keyboard: true // Кнопка з'являється лише один раз
+            resize_keyboard: true,
+            one_time_keyboard: true
         }
     });
 });
 
-// Налаштування CORS та підключення статичних файлів
 app.set('trust proxy', 1);
 app.use(cors({
     origin: allowedOrigins,
     credentials: true,
 }));
 
-// Роут для основної сторінки
 app.get('/', (req, res) => {
     res.send('Server is working!');
 });
 
-// Для локалізацій
 app.use('/locales', express.static('public/locales'));
 
-// Middleware для обробки JSON та cookie
 app.use(express.json());
 app.use(cookieParser());
-
-// API маршрути
 app.use('/api', router);
 
-// Підключення до MongoDB
 const start = async () => {
     try {
         await mongoose.connect(process.env.DB_URL, { useUnifiedTopology: true, useNewUrlParser: true });
